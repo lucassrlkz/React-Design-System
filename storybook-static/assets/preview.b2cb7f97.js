b315334ee42b8c975e842c6a13f1046238a33c8b
lucassrlkz <lucas_vieira97@hotmail.com>
2022-10-13 17:57:05 -0300

    ci: add publish storybook workflow

---
+++ storybook-static/assets/preview.b2cb7f97.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.6356014f.js";import"./iframe.291ab383.js";import"./es.map.constructor.b158b854.js";import"./es.number.to-fixed.592c5807.js";import"./index.113da5d7.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.b2cb7f97.js.map
