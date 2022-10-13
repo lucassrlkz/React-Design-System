ea5baac9ac74c8005f9918f2e1e57abcf47fbeef
lucassrlkz <lucas_vieira97@hotmail.com>
2022-10-13 13:32:00 -0300

    create a project with vite, tailwind, storybook, radix

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css"
+
+export function App() {
+	return (
+		<>
+			<h1 className="text-2xl">hello world</h1>
+			<button className="bg-cyan-500 font-bold px-4 py-2 rounded text-white hover:bg-cyan-300">
+				Enviar
+			</button>
+		</>
+	)
+}
---
+++ src/components/text/Text.stories.tsx
@@ -0,0 +1,47 @@
+import { Text, TextProps } from "./Text"
+import { Meta, StoryObj } from "@storybook/react"
+
+export default {
+	title: "Components/Text",
+	component: Text,
+	args: {
+		children: "Lorem ipsum",
+		size: "md",
+	},
+	argTypes: {
+		size: {
+			options: ["sm", "md", "lg"],
+			control: {
+				type: "inline-radio",
+			},
+		},
+	},
+} as Meta<TextProps>
+
+export const Default: StoryObj = {}
+
+export const Small: StoryObj<TextProps> = {
+	args: {
+		size: "sm",
+	},
+}
+
+export const Large: StoryObj<TextProps> = {
+	args: {
+		size: "lg",
+	},
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+	args: {
+		asChild: true,
+		children: <p>text with paragraph tag</p>,
+	},
+	argTypes: {
+		children: {
+			table: {
+				disable: true,
+			},
+		},
+	},
+}
